8912b3a869ddf395fc8ae144df2d101a8bab6416
Jammu-123 <113448291+Jammu-123@users.noreply.github.com>
2022-11-26 23:02:44 +0530

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+
+python -m Oslash_Calc sample_input\input1.txt
+python -m Oslash_Calc sample_input\input2.txt
+python -m Oslash_Calc sample_input\input3.txt
+python -m Oslash_Calc sample_input\input4.txt
